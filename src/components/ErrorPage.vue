b109a43020e1c9d189225339640767940e56764d
Ijeoma <106626684+ijayyyy@users.noreply.github.com>
2023-03-19 02:14:26 +0000

    made some changes to the store.js

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <center>
+     <div class='error'>
+       <div class="error-container">
+        <h1 class="error-h">404 Error!</h1>
+        <p class="oops">Oooops, it seems you are lost..</p>
+        <router-link to="/">Go to Home Page</router-link>
+     </div>
+    </div>
+  </center>
+   
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style>
+  .error {
+    background-color: #222;
+    height: 100vh;
+    color: white;
+  }
+  .error-container {
+    padding:100px;
+  }
+  .oops {
+    padding: 30px;
+  }
+    
+</style>
---
+++ src/main.js
@@ -0,0 +1,40 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import store from './store';
+import HomePage from "@/components/HomePage.vue";
+import AboutPage from "@/components/AboutPage.vue";
+import ErrorPage from "@/components/ErrorPage.vue";
+import MyCounter from "@/components/MyCounter.vue";
+import './assets/main.css'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "HomePage",
+      component: HomePage,
+    },
+    {
+      path: "/AboutPage",
+      name: "AboutPage",
+      component: AboutPage,
+    },
+    {
+      path: "/:pathName(.*)",
+      name: "ErrorPage",
+      component: ErrorPage,
+    },
+    {
+      path: "/MyCounter",
+      name: "MyCounter",
+      component: MyCounter,
+    }
+  ],
+});
+
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.mount('#app')
